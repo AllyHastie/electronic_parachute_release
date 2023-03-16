36d3a8b69f447ce0faf12ef7e3d6604a3c0e7d53
Alicia Hastie <aliciahastie@Alicias-MacBook-Air.local>
2023-03-17 09:39:10 +1100

    feature/updated_sensor_file_name

---
+++ src/barometer.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+Set up for BMP280
+******************************************************************************/
+
+
+#include <Adafruit_Sensor.h>
+#include <Adafruit_I2CDevice.h>
+#include <SPI.h>
+
+/******************************************************************************
+Function Name: XXXX
+Initialise I2C connection for barometer data transfer. 
+    Input: N/A
+    Output: N/A
+******************************************************************************/
+
+
+
+/******************************************************************************
+Function Name: XXXX
+Function reads in 100 readings from the barometer and averages readings to 
+reduce noise. Kalmann filter also completes this function but may be too 
+complex for this application. 
+    Input: N/A
+    Output: float avgAlt
+******************************************************************************/
+
+
+
+/******************************************************************************
+Function Name: XXXX
+Read in parachute deployment threshold and determine deployment altitude.
+    Input: int threshold, float armedAlt
+    Output: float deployAlt
+******************************************************************************/
+
+
+
+/******************************************************************************
+Function Name: XXXX
+Validate working connection with barometer.
+    Input: N/A
+    Output: boolean
+        0 - No error
+        1 - No reading and/or error
+******************************************************************************/
