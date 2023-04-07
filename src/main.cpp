11fd4140ea1652a2cc3cfaf4a73e3005c834e567
Alicia Hastie <aliciahastie@Alicias-Air-2.modem>
2023-04-07 14:32:40 +1000

    feature/eeprom

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
 #include <Arduino.h>
 #include "global_variables.h"
-
+#include "nonvolatile_memory.h"
 NVM nvm;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  Data data;
+  data.acceleration = {89.6,-6.8,32.4};
+  nvm.clearEEPROM();
+  for (int i = 0; i < 3; i++)
+  {
+    data.altitude = i + 10;
+    nvm.writeEEPROM(data, &nvm.startAddress);
+    Serial.println();
+    Serial.println();
+  }
+
+  nvm.readEEPROM();
+
+
 }
 
 void loop() {
